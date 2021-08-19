53b09ff5bf2e5cf335e7bfae56b934d5c161eb65
Araf Karsh Hamid <araf.karsh@metamagic.in>
2021-08-19 10:34:10 +0530

    Added Build no. and Build Date v0.2.0 to Swagger UI

--- src/main/java/io/fusion/air/microservice/server/config/ServiceHelp.java
+++ src/main/java/io/fusion/air/microservice/server/config/ServiceHelp.java
@@ -53,7 +53,7 @@ public class ServiceHelp {
 	
 	public static final String DL = "----------------------------------------------------------------------------";
 	
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.2.0";
 	
 	public static final String LOGO = "" +NL
 	+"============================================================================" + NL
